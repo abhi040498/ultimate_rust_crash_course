3bb3afdd84740b23223eec4e09084cc2561f2657
abhi040498 <abhishekpandey04041997@gmail.com>
2023-04-06 01:16:24 +0530

    Initial commit

---
+++ exercise/f_structs_traits/target/debug/deps/f_structs_traits.d
@@ -0,0 +1,5 @@
+D:\Development\rustDevelopment\ultimate_rust_crash_course\exercise\f_structs_traits\target\debug\deps\f_structs_traits.exe: src\main.rs
+
+D:\Development\rustDevelopment\ultimate_rust_crash_course\exercise\f_structs_traits\target\debug\deps\f_structs_traits.d: src\main.rs
+
+src\main.rs:
